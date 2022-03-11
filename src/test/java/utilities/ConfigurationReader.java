72bdf2e9aadcc0475a66a56cf1aff7985b86e394
Nadi8 <nadia.erdinieva@gmail.com>
2022-03-11 01:08:42 -0500

    update

--- src/test/java/utilities/ConfigurationReader.java
+++ src/test/java/utilities/ConfigurationReader.java
@@ -4,14 +4,14 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ConfigurationReader {
-	//configurationReader = default value is null 
+	
 			static Properties property;
 
-			//"try catch" opens and loads data 
+			
 			static {
 
 				try {
-					//this is declaring/telling the path of the properties file 
+					
 					String filePath = "src\\test\\resources\\testData\\configuration.properties";
 					FileInputStream input = new FileInputStream(filePath);
 					
@@ -26,7 +26,7 @@ public class ConfigurationReader {
 
 				}
 
-			} // closing static bracket 
+			} 
 			
 			public static String getProperty(String keyName) {
 				return property.getProperty(keyName);
@@ -34,4 +34,4 @@ public class ConfigurationReader {
 				
 			}
 
-		} // class level 
+		} 
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -10,62 +10,62 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
 
-	// declaring WebDriver but its value is null
+	
 	public static WebDriver driver;
 
-	// initializing WebDriver
+	
 	public static WebDriver getDriver() {
-		// if else statement allows not to repeat
+		
 		if (driver == null) {
-			// switch statement calling browser from configuration properties file
+			
 			switch (ConfigurationReader.getProperty("browser")) {
 
 			case "firefox":
-				// code
+				
 				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
 				driver.get(ConfigurationReader.getProperty("url"));
 				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				driver.manage().window().maximize();
-				// initializes the pages by calling them
+				
 				PageInitializer.initialize();
 				break;
 			case "chrome":
-				// code to setup Boni Garcia
+				
 				WebDriverManager.chromedriver().setup();
-				// we declare
+				
 				driver = new ChromeDriver();
 
 				driver.get(ConfigurationReader.getProperty("url"));
 				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				driver.manage().window().maximize();
-				// initializes the pages by calling them
+				
 				PageInitializer.initialize();
 
 				break;
 			case "edge":
-				// code
+				
 				WebDriverManager.edgedriver().setup();
 				driver = new EdgeDriver();
 				driver.get(ConfigurationReader.getProperty("url"));
 				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				driver.manage().window().maximize();
-				// initializes the pages by calling them
+				
 				PageInitializer.initialize();
 
 				break;
 			case "headless":
-				// code
+				
 				break;
 
-			} // switch
+			} 
 
-		} // end of 'if else'
-			// return driver
+		} 
+			
 		return driver;
-	} // end of getDriver method
+	} 
 
-	//
+	
 	public static void quitDriver() {
 		if (driver != null) {
 			driver.close();
@@ -74,4 +74,4 @@ public class Driver {
 
 	}
 
-} // end of class
+} 
